d52c94a9a07380206bd5040afc8fc74331990be0
MONTILLET Xavier <xavierm02.net@gmail.com>
2013-12-03 01:39:22 +0100

    Made MG work and removed ArrayMethods.

--- src/MisraGries.ml
+++ src/MisraGries.ml
@@ -1,13 +1,11 @@
-open ArrayMethods;;
-
-type ('a, 'b) non_empty_counter = {
-  key: 'a;
-  value: 'b
+type non_empty_counter = {
+  key: string;
+  mutable value: int
 };;
 
-type ('a, 'b) counter =
+type counter =
   | Empty
-  | Value of ('a, 'b) non_empty_counter
+  | Value of non_empty_counter
 ;;
 
 let make key value =
@@ -17,95 +15,75 @@ let make key value =
   })
 ;;
 
+let string_of_counter = function
+  | Value c -> c.key ^ " -> " ^ (string_of_int c.value)
+  | Empty -> "? -> 0"
+;;
+
+let string_of_counters counters =
+  (Array.fold_left
+    (fun acc counter -> acc ^ "  " ^ (string_of_counter counter) ^ "\n")
+    "[\n"
+    counters
+  ) ^ "]\n"
+;;
+
 let init k =
   Array.make k Empty
 ;;
 
+exception Done;;
+
 let read counters host =
-  let index = ArrayMethods.first_index
-    (fun counter -> match counter with
-      | Value c -> c.key = host
-      | Empty -> true
-    )
-    counters
-  in
-  match index with
-    | Some i -> begin
-      match counters.(i) with
-        | Value c -> incr c.value
-        | Empty -> counters.(i) <- make host (ref 1)
-    end
-    | None -> begin
-      ignore (ArrayMethods.first_index
-        (fun counter -> match counter with
-          | Value c -> begin
-            decr c.value;
-            false
-          end
-          | Empty -> true
-        )
-        counters
+  let increment_host_counter counters host =
+    Array.iter
+      (fun counter -> match counter with
+        | Value c ->
+          if c.key = host then begin
+            c.value <- c.value + 1;
+            raise Done
+          end else ()
+        | Empty -> ()
       )
-    end
-;;
-(*
-
-
-
-
-
-
-
-
-let drec_compteur c =
-  match c with
-    |Compte c0 -> (if (c0.valeur = 1) then Empty else Compte{clef = c0.clef; valeur = c0.valeur -1})
-    |_ -> Empty
-;;
-
-let rec decrAll cl =
-  match cl with
-    |t::q ->  (drec_compteur t) :: (decrAll q)
-    | _ -> cl
-;;
-
-let rec add cl k =
-(* to do : gérer le cas où il faut renvoyer decrAll *)
-    match cl with
-      |t::q ->
-        (match t with
-          |Compte c -> if c.clef = k then Compte {clef = k; valeur = c.valeur + 1} :: q else t :: add q k
-          |Empty -> Compte {clef=k;valeur=1} :: q)
-      |_ -> cl
-;;
-
-let rec misraGries f cl =
-  match f with
-    |t::q -> misraGries q (add cl t)
-    |_ -> cl
+      counters
+  in
+  let increment_empty_counter counters host =
+    Array.iteri
+      (fun index counter -> match counter with
+        | Value c -> ()
+        | Empty -> begin
+          counters.(index) <- make host 1;
+          raise Done
+        end
+      )
+      counters
+  in
+  let decrement_all_counters counters =
+    Array.iteri
+      (fun index counter -> match counter with
+        | Value c ->
+          if c.value > 1 then
+            c.value <- c.value - 1
+          else
+            counters.(index) <- Empty
+        | Empty -> ()
+      )
+      counters
+  in
+  try
+    increment_host_counter counters host;
+    increment_empty_counter counters host;
+    decrement_all_counters counters
+  with
+    | Done -> ()
 ;;
 
-let rec print cl =
-  match cl with
-    |t::q ->
-      (match t with
-        |Compte c -> begin
-                print_int c.clef;
-                print_string " ; ";
-                print_int c.valeur;
-                print_newline();
-                print q;
-              end
-        |Empty -> begin
-                print_string "Empty";
-                print_newline();
-                print q;
-              end )
-    |_ -> ()
+let read_all counters host_enum =
+  Enum.iter (read counters) host_enum
 ;;
 
-
-let cl = [Empty;Empty;Empty];;
-let f = [10;20;10;20;30;10;40];;
-print (misraGries f cl);;
-*)
+let misra_gries k host_enum =
+  let counters = init k in
+  read_all counters host_enum;
+  counters
+;;
--- src/main.ml
+++ src/main.ml
@@ -1,3 +1,8 @@
-open Enum;;
 open Logs;;
 open MisraGries;;
+
+let (|>) x f = f x;;
+
+let k = 10;;
+
+misra_gries k enum_1 |> string_of_counters |> print_string;;
