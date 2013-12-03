c502eafe8734629e5e09306bbace88f95ba250fc
MONTILLET Xavier <xavierm02.net@gmail.com>
2013-12-03 01:44:01 +0100

    Added tests un main.ml.

--- src/main.ml
+++ src/main.ml
@@ -6,3 +6,5 @@ let (|>) x f = f x;;
 let k = 10;;
 
 misra_gries k enum_1 |> string_of_counters |> print_string;;
+misra_gries k enum_2 |> string_of_counters |> print_string;;
+misra_gries k enum_3 |> string_of_counters |> print_string;;
